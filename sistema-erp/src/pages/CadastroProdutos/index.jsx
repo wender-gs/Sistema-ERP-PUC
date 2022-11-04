063c03759d6ccb41adb3c6a26f9934f8c378e2a4
wender-gs <wendergs2@gmail.com>
2022-11-04 13:24:33 -0300

    Preparando o código para ser trabalhado

---
+++ sistema-erp/src/pages/CadastroProdutos/index.jsx
@@ -0,0 +1,14 @@
+// TODO: Criar página de dashboard, responsaveis pela tarefa será definido.
+
+// Vamos tentar manter um padrão de código, limpo e objetivo, como não usaremos TS,
+// faço necessário o uso de comentarios, breves e objetivos explicando o que o trecho
+// do código faz, trazendo assim a facilidade de entendimento por outros desenvolvedores
+// que venham a participar do código.
+
+// toda regra de estilo devera ser escrita no
+//arquivo index.css na pasta de sua respectiva página
+import "./index.css";
+
+export const CadastroProdutos = () => {
+  return <span>Página para o cadastro de produtos a ser implementada</span>;
+};
---
+++ sistema-erp/src/pages/DispalayInsumos/index.jsx
@@ -0,0 +1,14 @@
+// TODO: Criar página de dashboard, responsaveis pela tarefa será definido.
+
+// Vamos tentar manter um padrão de código, limpo e objetivo, como não usaremos TS,
+// faço necessário o uso de comentarios, breves e objetivos explicando o que o trecho
+// do código faz, trazendo assim a facilidade de entendimento por outros desenvolvedores
+// que venham a participar do código.
+
+// toda regra de estilo devera ser escrita no
+//arquivo index.css na pasta de sua respectiva página
+import "./index.css";
+
+export const DisplayInsumos = () => {
+  return <span>Página para exibir e listar os insumos a ser implementada</span>;
+};

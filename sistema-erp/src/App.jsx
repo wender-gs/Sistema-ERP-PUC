bed8ea2eb6e882b92a3238a1b5b58e9442ddc990
Cirovsky <cirovisqui@gmail.com>
2022-11-07 20:58:57 -0300

    Inserindo a expressão "Import React ..."

--- sistema-erp/src/App.jsx
+++ sistema-erp/src/App.jsx
@@ -1,5 +1,6 @@
-import "./App.css";
-import { Login } from "./pages/Login";
+import "./App.css"
+import { Login } from "./pages/Login"
+import React from 'react'
 
 export const App = () => {
   return <Login />;

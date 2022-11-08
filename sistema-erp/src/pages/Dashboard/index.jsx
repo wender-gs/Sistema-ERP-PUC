137c0194494d3dff7cf48f910e44a6e402372b1f
carol-rdg <103288594+carol-rdg@users.noreply.github.com>
2022-11-07 21:48:53 -0300

    Update index.jsx

--- sistema-erp/src/pages/Dashboard/index.jsx
+++ sistema-erp/src/pages/Dashboard/index.jsx
@@ -12,3 +12,37 @@ import "./index.css";
 export const Dashboard = () => {
   return <span>Página de dashboard a ser implementada</span>;
 };
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+      
+    <title>Dashboard</title>
+</head>
+    <h1>Dashboard</h1>
+<body>
+    <input type="checkbox" id="check"> 
+    <label id="icone" for="check">
+        <img class="img-menu" src="/Wireframes/img/menu2.svg" alt=""> 
+    </label>
+    <div class="barra"> 
+    <nav> 
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Financeiro</a></li>
+            <li><a href="">Insumos</a></li>
+            <li><a href="">Produtos</a></li>
+        </ul>
+    </nav>
+    <header>
+           
+       
+      </header>
+    </div>
+  </body>
+</html>
